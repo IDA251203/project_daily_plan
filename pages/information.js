d78ffebbc07101e8346807e5f7dde6fde125d723
IDA251203 <dinaraazamatovna03@gmail.com>
2023-02-16 10:51:29 +0500

    changes

--- pages/information.js
+++ pages/information.js
@@ -4,27 +4,27 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Information = () => {
-  const name= localStorage.getItem("name");
-  const lastname=localStorage.getItem("lastname");
+  const name = window.localStorage.getItem("name");
+  const lastname = window.localStorage.getItem("lastname");
 
   return (
     <div className={styles.container}>
-        <div className={styles.box}>
-            <h1>Pricing Calculator</h1>
-            <p className={styles.text1}>Setup Daily Plans</p>
-        </div>
-        
-        
+      <div className={styles.box}>
+        <h1>Pricing Calculator</h1>
+        <p className={styles.text1}>Setup Daily Plans</p>
+      </div>
 
-        <div className={styles.box}>
-          <p className={styles.text3}>{name}</p>
-          <p className={styles.text4}>{lastname}</p>
-        </div>
 
-        <div className={styles.box}>
-            <div className={styles.profile}></div>
 
-        </div>
+      <div className={styles.box}>
+        <p className={styles.text3}>{name}</p>
+        <p className={styles.text4}>{lastname}</p>
+      </div>
+
+      <div className={styles.box}>
+        <div className={styles.profile}></div>
+
+      </div>
     </div>
   )
 }
